e7357552eadd0c2611f474180f58a04e64c54d9f
trinh04042000 <43408470+trinh04042000@users.noreply.github.com>
2018-11-01 15:40:24 +0700

    Add files via upload

---
+++ test/Animals/Chicken.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once(dirname( __FILE__ ) . '/../AbstractClass/Animal.php');
+include_once(dirname(__FILE__) . '/../InterfaceClass/Edible.php');
+
+class Chicken extends Animal
+{
+    public function makeSound()
+    {
+        return "Chicken: cluck-cluck!";
+    }
+}
